32abc7e6f6ada632eed76ddae4f53e578336a0fd
sangyoo91 <sangyoo.91@gmail.com>
2015-06-11 15:14:27 +0900

    Add New Header

--- apps/front/client/templates/0/layout/layout.js
+++ apps/front/client/templates/0/layout/layout.js
@@ -22,6 +22,13 @@ Template.layout.events({
     } else {
 
     }
+  },
+  'click [data-action=signIn]': function() {
+    Accounts.ui.dialog("_signInDialogContent");
+  },
+  'click [data-action=signOut]': function() {
+    Meteor.logout();
+    Router.go('home');
   }
 });
 
